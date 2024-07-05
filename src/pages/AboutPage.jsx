6a53076f4a97c8e65c14bc49b9fe22bf547edc2e
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-07-05 16:59:28 -0700

    Add additional padding for anchor tags on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -27,11 +27,11 @@ export default function AboutPage() {
           The road ahead is still long, but with a solid foundation in
           full-stack web development, I am excited to tackle the challenges and
           opportunities that lie ahead. My expertise spans both
-          <a class="font-extrabold hover:text-emerald-400" href="">
+          <a class="font-extrabold hover:text-emerald-400 px-1" href="">
             front-end
           </a>
           and
-          <a class="font-extrabold hover:text-emerald-400" href="">
+          <a class="font-extrabold hover:text-emerald-400 px-1" href="">
             back-end
           </a>
           technologies, enabling me to create seamless, efficient, and dynamic
@@ -39,11 +39,11 @@ export default function AboutPage() {
         </p>
         <p class="text-base md:text-2xl text-center py-8">
           When I'm not a the computer I like to
-          <a class="text-red-800" href="https://www.olympiacos.org/en/">
+          <a class="text-red-800 pl-1 font-bold" href="https://www.olympiacos.org/en/">
             play soccer
           </a>
-          play soccer, go cycling and hang out with my dog getting lost in
-          Briths Columbia's endless hiking trails.
+          , go cycling and hang out with my dog getting lost in
+          British Columbia's endless hiking trails.
         </p>
       </div>
     </main>
