e7824c9147b9940116eb9c042bb2e657a4522485
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-07-08 15:58:05 -0700

    Change font on about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -16,7 +16,7 @@ export default function AboutPage() {
           a new software engineering journey, culminating in my graduation with
           a
           <a
-            class="font-extrabold hover:text-emerald-400 pl-1"
+            class="text-slate-100 underline font-extrabold hover:text-emerald-400 pl-1"
             href="https://www.lighthouselabs.ca/en/web-development"
           >
             Web Development diploma
@@ -27,11 +27,11 @@ export default function AboutPage() {
           The road ahead is still long, but with a solid foundation in
           full-stack web development, I am excited to tackle the challenges and
           opportunities that lie ahead. My expertise spans both
-          <a class="font-extrabold hover:text-emerald-400 px-1" href="">
+          <a class="text-slate-100 underline font-extrabold hover:text-emerald-400 px-1" href="">
             front-end
           </a>
           and
-          <a class="font-extrabold hover:text-emerald-400 px-1" href="">
+          <a class="font-extrabold text-slate-100 underline hover:text-emerald-400 px-1" href="">
             back-end
           </a>
           technologies, enabling me to create seamless, efficient, and dynamic
@@ -39,7 +39,7 @@ export default function AboutPage() {
         </p>
         <p class="text-base md:text-2xl text-center py-8">
           When I'm not a the computer I like to
-          <a class="text-red-800 pl-1 font-bold" href="https://www.olympiacos.org/en/">
+          <a class="text-red-500 underline pl-1 font-bold" href="https://www.olympiacos.org/en/">
             play soccer
           </a>
           , go cycling and hang out with my dog getting lost in
