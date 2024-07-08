7c303af7e9f69e21d7e98c75de86c0ed4db57a7a
Dimitrios Athanasopoulos <dimi1086@hotmail.com>
2024-07-07 18:42:24 -0700

    Add contact info and email button

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { FiSend } from "react-icons/fi";
 
 export default function ContactPage() {
   useEffect(() => {
@@ -7,7 +8,18 @@ export default function ContactPage() {
 
   return(
     <div>
-      This is a contact page.
+      <main class="flex-1">
+        <h1 class="text-3xl md:text-6xl py-10 text-center">GET IN TOUCH</h1>
+        <div class="flex text-center">
+          <FiSend size={60} className="mx-auto" />
+        </div>
+        <p class="text-base md:text-2xl text-center py-8">
+          Whether you want to chat or have an idea for a project, feel free to send me an email!
+        </p>
+        <div class="text-base md:text-2xl text-center py-8">
+            <button className="border-solid border-2 border-sky-500 p-3 hover:bg-white hover:text-slate-800">Say Hello!</button>
+        </div>
+      </main>
     </div>
   )
 }
